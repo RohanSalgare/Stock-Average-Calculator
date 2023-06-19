250a44ef72fb63810e5e5e6d4c7589bc64032f88
DELL <rohansalgare4@gmail.com>
2023-06-19 12:32:28 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Stock Average Calculator and SIP Calculator
+## Introduction
+This project consists of two calculators: the Stock Average Calculator and the SIP (Systematic Investment Plan) Calculator. These calculators are designed to assist users in calculating the average price of stocks and estimating the returns from a SIP investment, respectively. This README file provides an overview of the project, instructions on how to use the calculators, and details about the implementation.
+
+## Features
+The Stock Average Calculator allows users to calculate the average price of stocks based on the total investment amount and the quantity of stocks purchased at different prices. It helps investors determine their average cost per share, which is useful for evaluating their investment performance.
+
+The SIP Calculator enables users to estimate the returns from a Systematic Investment Plan. By providing the investment amount, duration, expected annual growth rate, and investment frequency, users can get an approximation of the total investment value and the potential returns on their SIP investment.
+
+## How to Use
+To use the Stock Average Calculator, follow these steps:
+
+1. For each trade, enter the quantity and price per stock.
+2. The calculator will display the average price per stock based on the provided information.
+3. It will also calculate the Total quantity and Total investment
+
+To use the SIP Calculator, follow these steps:
+
+1. Input the investment amount.
+3. Enter the investment duration in years.
+4. Provide the expected annual growth rate (in percentage).
+5. The calculator will provide an estimate of the total investment value and the potential returns.
+
+
+Conclusion
+The Stock Average Calculator and SIP Calculator provide useful tools for investors to analyze their stock investments and estimate the potential returns from SIP investments. Following the instructions above, users can easily utilize these calculators and make informed investment decisions.
+
+
+
+
+
---
+++ src/scritp.js
@@ -0,0 +1,107 @@
+const intlLanguage = Intl.DateTimeFormat().resolvedOptions().timeZone === 'Asia/Calcutta' ? 'en-IN' : 'en-US';
+
+function calculateSIP() {
+
+    var investment = parseInt(document.getElementById("monthlyInvestment").value);
+    var rateOfInterest = parseInt(document.getElementById("rateOfInterest").value);
+    var years = parseInt(document.getElementById("years").value);
+    var monthlyRate = rateOfInterest / 12 / 100;
+    var months = years * 12;
+    var totalGain = (investment * (Math.pow((1 + monthlyRate), months) - 1) / monthlyRate * (1 + monthlyRate));
+
+    if (investment && rateOfInterest && years) {
+        document.getElementById("inam").innerHTML = Number(investment * months).toFixed(2).toLocaleString(intlLanguage);
+        document.getElementById("estam").innerHTML = Number(totalGain - (investment * months)).toFixed(2).toLocaleString(intlLanguage);
+        document.getElementById("totv").innerHTML = Number(totalGain.toFixed(2)).toLocaleString(intlLanguage);
+    }
+    else{
+        alert("Enter values of all Fields");
+    }
+
+}
+
+
+let flag = "ave";
+var SIPview = document.getElementById("SIP-cal");
+var aveView = document.getElementById("ave-cal");
+
+var sipLabel = document.getElementById("sipcal");
+var aveLabel = document.getElementById("avecal");
+
+function switchToSIP() {
+
+    if (flag == "ave") {
+
+        aveView.classList.remove("block");
+        aveView.classList.add("hidden");
+
+        SIPview.classList.remove("hidden");
+        SIPview.classList.add("block");
+        flag = "SIP";
+
+
+    }
+    aveLabel.classList.remove("text-blue-400");
+    sipLabel.classList.add("text-blue-400");
+
+}
+
+function switchToAve() {
+
+    if (flag == "SIP") {
+        SIPview.classList.remove("block");
+        SIPview.classList.add("hidden");
+
+        aveView.classList.remove("hidden");
+        aveView.classList.add("block");
+
+        flag = "ave";
+    }
+    sipLabel.classList.remove("text-blue-400");
+    aveLabel.classList.add("text-blue-400");
+
+
+}
+
+function calculateAve() {
+    let firstPrice = parseInt(document.getElementById("current-price").value);
+    let newPrice = parseInt(document.getElementById("new-price").value);
+    let firstBuyQuantity = parseInt(document.getElementById("first-units").value);
+    let newBuyQuantity = parseInt(document.getElementById("second-units").value);
+
+    let totalQuantity = firstBuyQuantity + newBuyQuantity;
+    let totalInvestment = (firstBuyQuantity * firstPrice) + (newBuyQuantity * newPrice);
+
+    if (firstPrice && newPrice && firstBuyQuantity && newBuyQuantity) {
+        document.getElementById("totq").innerHTML = Number(totalQuantity).toLocaleString(intlLanguage);
+        document.getElementById("avep").innerHTML = Number(totalInvestment / totalQuantity).toFixed(2).toLocaleString(intlLanguage);
+        document.getElementById("toti").innerHTML = Number(totalInvestment).toLocaleString(intlLanguage);
+        document.getElementById("totif").innerHTML = Number(firstPrice*firstBuyQuantity).toLocaleString(intlLanguage);
+        document.getElementById("totis").innerHTML = Number(newPrice*newBuyQuantity).toLocaleString(intlLanguage);
+    }
+    else{
+        alert("Enter Values of all Fields");
+    }
+}
+
+function ClearFields() {
+    if (flag == "ave") {
+        document.getElementById("current-price").value = "";
+        document.getElementById("new-price").value = "";
+        document.getElementById("first-units").value = "";
+        document.getElementById("second-units").value = "";
+        document.getElementById("totq").innerHTML =0;
+        document.getElementById("avep").innerHTML = 0;
+        document.getElementById("toti").innerHTML =0;
+    }
+
+    if (flag == "SIP") {
+        document.getElementById("monthlyInvestment").value = "";
+        document.getElementById("rateOfInterest").value = "";
+        document.getElementById("years").value = "";
+        document.getElementById("inam").innerHTML =0;
+        document.getElementById("estam").innerHTML = 0;
+        document.getElementById("totv").innerHTML =0;
+    }
+}
+
